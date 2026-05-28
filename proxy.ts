ef29ffedafbdda863380d1e03d97a70b6160a7a3
KB.KIM <kb1020.kim@gmail.com>
2026-05-28 15:52:28 +0900

    modified npm install

--- proxy.ts
+++ proxy.ts
@@ -1,7 +1,7 @@
 import { getToken } from "next-auth/jwt"
 import { NextResponse, type NextRequest } from "next/server"
 
-export default async function middleware(req: NextRequest) {
+export default async function proxy(req: NextRequest) {
   const token = await getToken({
     req,
     secret: process.env.AUTH_SECRET ?? process.env.NEXTAUTH_SECRET,
